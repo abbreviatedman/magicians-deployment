c473635139d6c82f2e62d868cb11f33461b0ab1c
Colin Jaffe <balloonasaurus@gmail.com>
2023-01-31 12:01:02 -0500

    Adds full solution code.

--- app.py
+++ app.py
@@ -32,8 +32,47 @@ def post_data_to_json(filename, data):
     with open(filename, "w") as file:
         json.dump(data, file, indent=2)
 
-# YOUR CODE GOES BELOW
+@app.get("/characters")
+def get_characters():
+    return get_data_from_json("characters.json")
 
-@app.get("/hey")
-def say_hi():
-    return "Hello, World!"
+@app.get("/characters/<string:id>")
+def get_character(id):
+    characters = get_data_from_json("characters.json")
+    for character in characters:
+        if character["id"] == int(id):
+            return character
+
+    return 404
+
+@app.post("/characters")
+def add_character():
+    request_data = request.get_json()
+    characters = get_data_from_json("characters.json")
+    characters.append(request_data)
+    post_data_to_json("characters.json", characters)
+
+    return request_data, 201
+
+@app.get("/quotes")
+def get_quotes():
+    return get_data_from_json("quotes.json")
+
+@app.get("/quotes/<string:id>")
+def get_quote(id):
+    quotes = get_data_from_json("quotes.json")
+    for quote in quotes:
+        if quote["id"] == int(id):
+            return quote
+
+    return 404
+
+
+@app.post("/quotes")
+def add_quote():
+    request_data = request.get_json()
+    quotes = get_data_from_json("quotes.json")
+    quotes.append(request_data)
+    post_data_to_json("quotes.json", quotes)
+
+    return request_data, 201
